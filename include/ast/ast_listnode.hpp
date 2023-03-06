519d6f052c3c40028317fb43ee105b31ada4755a
Johan Jino <johanjinoj@gmail.com>
2023-03-06 15:10:53 +0000

    renaming functions for readability
    Co-Authored-By: Sohailul Islam Alvi <94545356+alvi-codes@users.noreply.github.com>

--- include/ast/ast_listnode.hpp
+++ include/ast/ast_listnode.hpp
@@ -11,15 +11,15 @@ typedef std::vector<NodePtr> List;
 typedef List *ListPtr;
 
 // Functions to be used in parser for instantiation / concatenation
-inline ListPtr init_list(NodePtr expr){
-  ListPtr exprList = new List(); // Potentially replace with Nodelist(NodePtr) constructor
-  exprList->push_back(expr);
-  return exprList;
+inline ListPtr init_list(NodePtr node){
+  ListPtr nodeList = new List(); // Potentially replace with Nodelist(NodePtr) constructor
+  nodeList->push_back(node);
+  return nodeList;
 }
 
-inline ListPtr concat_list(NodePtr expr, ListPtr exprList){
-  exprList->push_back(expr);
-  return exprList;
+inline ListPtr concat_list(NodePtr node, ListPtr nodeList){
+  nodeList->push_back(node);
+  return nodeList;
 }
 
 #endif
