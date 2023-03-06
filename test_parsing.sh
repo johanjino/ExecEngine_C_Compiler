a3b0004a19eca84b1f8c27f4e154c3ba8910b88a
Johan Jino <johanjinoj@gmail.com>
2023-03-06 15:13:48 +0000

    new `test_parsing.sh` to see how many were parsed
    calculates number of tests parsed without lex/parser error
    
    Co-Authored-By: Sohailul Islam Alvi <94545356+alvi-codes@users.noreply.github.com>

---
+++ test_parsing.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Initialize counters
+non_empty_count=0
+total_count=0
+
+# Loop through all .s files
+for file in bin/output/*.s; do
+    if [ -s "$file" ]; then
+        # File is not empty
+        ((non_empty_count++))
+    fi
+    ((total_count++))
+done
+
+# Echo results
+echo "Number of non-empty .s files: $non_empty_count"
+echo "Total number of .s files: $total_count"
